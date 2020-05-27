d67cad61cdfc80f8cbd453c935b23515a11be023
ktsyganova <Ksenia.Tsyganova@returnonintelligence.com>
2020-05-27 03:22:58 +0300

    crud

--- src/main/java/com/course/controllers/CarController.java
+++ src/main/java/com/course/controllers/CarController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,5 +44,17 @@ public class CarController {
     @ResponseStatus(HttpStatus.OK)
     public CarDto update(@PathVariable("carId") int id, @RequestBody CarDto carDto) {
         return carService.updateCar(id, carDto);
+    }
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public CarDto create(@RequestBody CarDto carDto) {
+        return carService.createCar(carDto);
+    }
+
+    @DeleteMapping(value = "/{carId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("carId") int id) {
+        carService.deleteCar(id);
     }
 }
--- src/main/java/com/course/services/CarService.java
+++ src/main/java/com/course/services/CarService.java
@@ -33,12 +33,29 @@ public class CarService {
     @Transactional
     public CarDto updateCar(int carId, CarDto updateDto) {
         CarEntity entity = carRepository.getOne(carId);
-        entity.setNumber(updateDto.getNumber());
-        entity.setColor(updateDto.getColor());
-        entity.setMark(updateDto.getMark());
-        entity.setIsForeign(updateDto.isForeign() ? 1 : 0);
+        fillEntityProperties(updateDto, entity);
         carRepository.save(entity);
         return toCarDto(carRepository.getOne(carId));
+    }
+
+    @Transactional
+    public CarDto createCar(CarDto createDto) {
+        CarEntity entity = new CarEntity();
+        fillEntityProperties(createDto, entity);
+        carRepository.save(entity);
+        return toCarDto(carRepository.getOne(entity.getId()));
+    }
+
+    @Transactional
+    public void deleteCar(int carId) {
+        carRepository.deleteById(carId);
+    }
+
+    private void fillEntityProperties(CarDto dto, CarEntity entity) {
+        entity.setNumber(dto.getNumber());
+        entity.setColor(dto.getColor());
+        entity.setMark(dto.getMark());
+        entity.setIsForeign(dto.isForeign() ? 1 : 0);
     }
 
     private CarDto toCarDto(CarEntity entity) {
